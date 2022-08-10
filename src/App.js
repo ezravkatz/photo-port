4fbade8b6955d1fcfd8dce178905bf5f5ef58982
ezravkatz <ezravkatz@gmail.com>
2022-08-10 14:53:18 -0400

    working on gallery

--- src/App.js
+++ src/App.js
@@ -1,14 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Nav from './components/Nav';
 import About from './components/About';
+import Gallery from './components/Gallery';
 
 function App() {
+  const [categories] = useState([
+    {
+      name: 'Portfolio',
+      description: 'Projects I have completed',
+    },
+    { name: 'Resume', description: 'View and download my resume.' },
+  ]); 
+
+  const [currentCategory, setCurrentCategory] = useState(categories[0]);
 
   return (
     <div>
-      <Nav />
+      <Nav
+        categories={categories}
+        setCurrentCategory={setCurrentCategory}
+        currentCategory={currentCategory}
+      ></Nav>
       <main>
-          <About />        
+        <Gallery currentCategory={currentCategory}></Gallery>
+        <About></About>
       </main>
     </div>
   );
